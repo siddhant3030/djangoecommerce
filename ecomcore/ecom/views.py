4ec9289b97633ec3a6342f79b831840e149490b6
siddhant3030 <ssiddhant3030@gmail.com>
2019-09-10 00:49:30 +0530

    changed some inside files

--- ecomcore/ecom/views.py
+++ ecomcore/ecom/views.py
@@ -7,6 +7,12 @@ from .models import Item
 #     }
 #     return render(request, "products.html", context)
 
+def item_list(request):
+    context = {
+        'items': Item.Objects.all()
+    }
+    return render(request, "home-page.html", context)
+
 def checkout(request):
     return render(request, "checkout.html")
 
