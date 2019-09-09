1741a7a85e366d1a67a5e960b1985fb9e80a7239
siddhant3030 <ssiddhant3030@gmail.com>
2019-09-09 12:46:43 +0530

    added old fields to new file

--- ecomcore/ecom/models.py
+++ ecomcore/ecom/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    title = models.CharField(max_length = 100)
+    price = models.FloatField()
+
+    def _str_(self):
+        return self.title
+
+class OrderItem(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    title = models.CharField(max_length = 100)
+    def _str_(self):
+        return self.title
+
+
+class Order(models.Model):
+    title = models.CharField(max_length = 100)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+    
+    def _str_(self):
+        return self.title
+
