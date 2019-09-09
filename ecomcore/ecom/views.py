b0877ccacac3b50b12f9b06bbac8678bf42479f7
siddhant3030 <ssiddhant3030@gmail.com>
2019-09-10 00:30:46 +0530

    mdbootstrap added in file

--- ecomcore/ecom/views.py
+++ ecomcore/ecom/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from .models import Item
 
-def products(request):
-    context = {
-        'items': Item.objects.all()
-    }
-    return render(request, "product.html", context)
+# def products(request):
+#     context = {
+#         'items': Item.objects.all()
+#     }
+#     return render(request, "products.html", context)
+
+def checkout(request):
+    return render(request, "checkout.html")
+
