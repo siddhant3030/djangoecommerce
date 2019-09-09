ea8d6443e75e304132d09d82785a06c0b1531147
siddhant3030 <ssiddhant3030@gmail.com>
2019-09-09 12:28:12 +0530

    added relationship between items and order

--- ecomm/models.py
+++ ecomm/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 
 class OrderItem(models.Model):
-    pass
+    title = models.CharField(max_length = 100)
+    def _str_(self):
+        return self.title
+
 
 class Order(models.Model):
-    pass
+    title = models.CharField(max_length = 100)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+    
+    def _str_(self):
+        return self.title
+
 
 class Item(models.Model):
     title = models.CharField(max_length = 100)
+
 
     def _str_(self):
         return self.title
