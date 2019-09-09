f0370cca4d6237fc3a7ec0190c570f727d6dc5d6
siddhant3030 <ssiddhant3030@gmail.com>
2019-09-09 12:19:50 +0530

    Added class for order and item

--- ecomm/models.py
+++ ecomm/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class OrderItem(models.Model):
+    pass
+
+class Order(models.Model):
+    pass
+
+class Item(models.Model):
+    title = models.CharField(max_length = 100)
+
+    def _str_(self):
+        return self.title
