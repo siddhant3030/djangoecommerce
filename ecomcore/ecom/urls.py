dffadc5d9251a4963539e93886f61bd8e869f11e
siddhant3030 <ssiddhant3030@gmail.com>
2019-09-12 01:07:33 +0530

    added slug and discount_price field in Item

--- ecomcore/ecom/urls.py
+++ ecomcore/ecom/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-#from views import (ItemDetailView, checkout, HomeView)
+from views import (ItemDetailView, checkout, HomeView)
 
 app_name = 'ecom'
 
-# urlpatterns = [
-#     path('', HomeView.as_view(), name='item-list'),
-#     path('checkout/', checkout, name='checkout'),
-#     path('product/<slug>/', ItemDetailView.as_view(), name='product')
-# ]
+urlpatterns = [
+    path('', HomeView.as_view(), name='item-list'),
+    path('checkout/', checkout, name='checkout'),
+    path('product/<slug>/', ItemDetailView.as_view(), name='product')
+]
 
